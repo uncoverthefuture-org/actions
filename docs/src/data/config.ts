740b6b9b1ec01ae416dd7e7a666b7ff3674ece58
Friday Davis <55281305+sirdavis99@users.noreply.github.com>
2026-03-27 01:42:54 +0100

    feat: migrate ui mapping arrays to config ts

---
+++ docs/src/data/config.ts
@@ -0,0 +1,43 @@
+import sshContainerDeploy from "./ssh_container_deploy.md?raw";
+
+export const UI_CONFIG = {
+  sidebarFrameUrl: "frames/sidebar",
+  docContentFrameUrl: "frames/doc_content",
+};
+
+export interface NavItem {
+    label: string;
+    route?: string;
+    children?: NavItem[];
+}
+
+export const navItems: NavItem[] = [
+    {
+        label: "Home",
+        route: "/"
+    },
+    {
+        label: "Packages",
+        children: [
+            {
+                label: "@uncover/actions",
+                children: [
+                    {
+                        label: "SSH Container Deploy",
+                        route: "/uncover-actions/ssh-container-deploy",
+                    },
+                ],
+            },
+        ],
+    },
+];
+
+export const titleMap: Record<string, string> = {
+    "/": "Documentation",
+    "/uncover-actions/ssh-container-deploy": "SSH Container Deploy",
+};
+
+export const contentMap: Record<string, string> = {
+    "/": "# Welcome to Uncover Actions Docs \n Select a function from the sidebar to view usage details.",
+    "/uncover-actions/ssh-container-deploy": sshContainerDeploy,
+};
